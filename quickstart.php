2773082ee00972e3d9a36ebfbc5e691665420f91
= <urazovm@hotmail.com>
2018-10-18 01:25:37 -0700

    modified

--- quickstart.php
+++ quickstart.php
@@ -1,26 +1,5 @@
 <?php
-/**
- * Copyright 2018 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-// [START sheets_quickstart]
 require __DIR__ . '/vendor/autoload.php';
-
-if (php_sapi_name() != 'cli') {
-    throw new Exception('This application must be run on the command line.');
-}
-
 /**
  * Returns an authorized API client.
  * @return Google_Client the authorized client object
@@ -77,8 +56,8 @@ $client = getClient();
 $service = new Google_Service_Sheets($client);
 
 // Prints the names and majors of students in a sample spreadsheet:
-// https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-$spreadsheetId = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms';
+// Google Sheet ID
+$spreadsheetId = '45xi8Vs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms';
 $range = 'Class Data!A2:E';
 $response = $service->spreadsheets_values->get($spreadsheetId, $range);
 $values = $response->getValues();
@@ -92,4 +71,3 @@ if (empty($values)) {
         printf("%s, %s\n", $row[0], $row[4]);
     }
 }
-// [END sheets_quickstart]
